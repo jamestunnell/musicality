43fbde2f206b3a6b72fd8d7a7b6569469c1705a1
James Tunnell <jamestunnell@gmail.com>
2014-12-11 12:40:09 -0800

    Update CompoundSequence to return enumerators when appropriate

--- lib/musicality/composition/util/compound_sequence.rb
+++ lib/musicality/composition/util/compound_sequence.rb
@@ -7,32 +7,32 @@ class CompoundSequence
   end
   
   def at offset
-    vals = @seqs.map {|s| s.at(offset) }
-    combine vals
+    if offset.is_a? Enumerable
+      return enum_for(:at,offset) unless block_given?
+      enums = @seqs.map {|s| s.at(offset) }
+      offset.size.times { yield combine(enums.map {|e| e.next }) }
+    else
+      vals = @seqs.map {|s| s.at(offset) }
+      return combine(vals)
+    end
   end
   
   def take n
+    return enum_for(:take,n) unless block_given?
     enums = @seqs.map {|s| s.take(n) }
-    Array.new(n) do
-      vals = enums.map {|e| e.next }
-      combine vals
-    end
+    n.times { yield combine(enums.map {|e| e.next }) }
   end
   
   def over range
+    return enum_for(:over,range) unless block_given?
     enums = @seqs.map {|s| s.over(range) }
-    Array.new(range.size) do
-      vals = enums.map {|e| e.next }
-      combine vals
-    end
+    range.size.times { yield combine(enums.map {|e| e.next }) }
   end
   
   def take_back n
+    return enum_for(:take_back,n) unless block_given?
     enums = @seqs.map {|s| s.take_back(n) }
-    Array.new(n) do
-      vals = enums.map {|e| e.next }
-      combine vals
-    end
+    n.times { yield combine(enums.map {|e| e.next }) }
   end
   
   private
--- spec/composition/util/compound_sequence_spec.rb
+++ spec/composition/util/compound_sequence_spec.rb
@@ -4,21 +4,43 @@ describe CompoundSequence do
   seqs = [ AddingSequence.new([1,1,0,2,-1]),
            RepeatingSequence.new([0,1,0,5,-13]) ]
   
-  { :at => [0, 1, -5, 33],
-    :take => [],
-    :take_back => [],
-    :over => []
+  describe "#at" do
+    context 'given single offset' do
+      [ :+, :* ].each do |sym|
+        cseq = CompoundSequence.new(sym,seqs)
+        context "given #{sym} as combine method" do
+          it "should get a value from each sequence using #at and combine using #{sym}" do
+            [0, 1, -5, 33].each do |offset|
+              vals = seqs.map {|s| s.at(offset) }
+              val = vals[1..-1].inject(vals.first,sym)
+              val2 = cseq.send(:at,offset)
+              val2.should eq(val)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  { :at => [[-5,1,33,2,0,-11], [-3,-2,-1,0,1,2,-3,-2,-1]],
+    :take => [ 0, 5, 10, 13 ],
+    :take_back => [ 0, 2, 6, 15 ],
+    :over => [ 0..12, -3...5, -10..-3 ]
   }.each do |method,args|
     describe "##{method}" do
       [ :+, :* ].each do |sym|
         cseq = CompoundSequence.new(sym,seqs)
         context "given #{sym} as combine method" do
-          it "should inject using #{sym} to combine the separate results from calling ##{method} on each sequence" do
+          it "should get values from each sequence using #{method} and combine each set of values using #{sym}" do
             args.each do |arg|
-              vals = seqs.map {|s| s.send(method,arg) }
-              val = vals[1..-1].inject(vals.first,sym)
-              val2 = cseq.send(method,arg)
-              val2.should eq(val)
+              enums = seqs.map {|s| s.send(method,arg) }
+              n = arg.is_a?(Fixnum) ? arg : arg.size
+              vals = Array.new(n) do |i|
+                _vals = enums.map {|e| e.next }
+                _vals[1..-1].inject(_vals.first,sym)
+              end
+              vals2 = cseq.send(method,arg).to_a
+              vals2.should eq(vals)
             end
           end
         end
