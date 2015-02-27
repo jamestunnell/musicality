d144b528e771f136b95208cc142673f66e52539f
James Tunnell <jamestunnell@gmail.com>
2015-02-26 16:59:33 -0800

    Tweaks to composition examples

--- examples/composition/auto_counterpoint.rb
+++ examples/composition/auto_counterpoint.rb
@@ -2,25 +2,45 @@ require 'musicality'
 include Musicality
 include Pitches
 
-durs_w_probs = { 1/8.to_r => 0.25, 1/4.to_r => 0.5, 1/2.to_r => 0.25 }
-#durs_w_probs = { 1/6.to_r => 0.25, 1/4.to_r => 0.25, 1/3.to_r => 0.25, 1/12.to_r => 0.25 }
-rrg = RandomRhythmGenerator.new(durs_w_probs)
-palette = durs_w_probs.keys
-
-bass_pitches = [ C2, D2, F2, E2, A2, E2, D2 ]
-guitar_pitches = [ F3, F3, A3, G3, C4, A3, G3 ]
+bass_pitch_palette = [ C2, D2, E2, F2, G2, A2, B2, C3 ]
+guitar_pitch_palette = [ C3, D3, E3, F3, G3, A3, B3, C4 ]
 
 bass = Part.new(Dynamics::MF)
 guitar = Part.new(Dynamics::MF)
 
-25.times do
-  rhythm = rrg.random_rhythm(1)
+def random_melody rhythm, pitch_palette
+  pitches = pitch_palette.sample(rand(2..pitch_palette.size))
+  pitch_sampler = RandomSampler.new(pitches,Probabilities.random(pitches.size))
+  make_notes(rhythm, pitch_sampler.sample(rhythm.size))
+end
   
-  cpg = CounterpointGenerator.new(rhythm,palette)
-  counterpoint = cpg.best_solution(1/12.to_r)
-  
-  bass.notes += make_notes(counterpoint, bass_pitches)
-  guitar.notes += make_notes(rhythm, guitar_pitches)
+def random_counterpoint rhythm, rhythm_palette, sample_rate, pitch_palette
+  cpg = CounterpointGenerator.new(rhythm,rhythm_palette)
+  counterpoint = cpg.best_solutions(25,0.5,sample_rate).sample
+  pitches = pitch_palette.sample(rand(2..pitch_palette.size))
+  pitch_sampler = RandomSampler.new(pitches,Probabilities.random(pitches.size))
+  make_notes(counterpoint, pitch_sampler.sample(counterpoint.size))  
+end
+
+2.times do
+  [ {
+      :rhythm_probs => { 1/8.to_r => 0.25, 1/4.to_r => 0.25, 1/6.to_r => 0.25, 1/2.to_r => 0.25 },
+      :sample_rate => 48,
+    },
+    {
+      :rhythm_probs => { 1/8.to_r => 0.325, 1/4.to_r => 0.325, 3/16.to_r => 0.10, 1/2.to_r => 0.25 },
+      :sample_rate => 16,
+    },
+  ].each do |params|
+    rrg =RandomRhythmGenerator.new(params[:rhythm_probs])
+    rhythm_palette = params[:rhythm_probs].keys
+    5.times do
+      rhythm = rrg.random_rhythm(3/4.to_r)
+      guitar.notes += random_melody(rhythm, guitar_pitch_palette)
+      bass.notes += random_counterpoint(rhythm, rhythm_palette,
+                                        params[:sample_rate], bass_pitch_palette)
+    end
+  end
 end
 
 score = Score::Unmeasured.new(120,
---
+++ examples/composition/part_generator.rb
@@ -0,0 +1,51 @@
+require 'musicality'
+include Musicality
+include Pitches
+include PitchClasses
+include Meters
+include ScaleClasses
+
+minor = Heptatonic::Prima::MINOR
+major = Heptatonic::Prima::MAJOR
+
+pitch_seqs = {
+  D4 => minor, C4 => major, E4 => minor, F4 => minor
+}.map do |pitch,scale_class|
+  scale_class.to_pitch_seq(pitch)        
+end
+
+rhythm_seq2 = RepeatingSequence.new([3/8.to_r]*4)
+
+score = Score::Measured.new(SIX_EIGHT,90) do |s|
+  s.parts["main"] = Part.new(Dynamics::MF) do |p|
+    rhythm_seq = RepeatingSequence.new(([1/8.to_r]*3)*3 + [1/4.to_r,1/8.to_r])
+    selector = RepeatingSequence.new([4,2,0])
+    rhythm = rhythm_seq.take(pitch_seqs.size * rhythm_seq.pattern_size).to_a
+    poffsets = selector.take(rhythm_seq.pattern_size).to_a
+    pitches = pitch_seqs.map { |pseq| pseq.at(poffsets).to_a }.flatten
+    p.notes += make_notes(rhythm,pitches)
+  end
+  
+  s.parts["bass"] = Part.new(Dynamics::MP) do |p|
+    rhythm_seq = RepeatingSequence.new([3/8.to_r]*4)
+    selector = RepeatingSequence.new([-7])
+    rhythm = rhythm_seq.take(pitch_seqs.size * rhythm_seq.pattern_size).to_a
+    poffsets = selector.take(rhythm_seq.pattern_size).to_a
+    pitches = pitch_seqs.map { |pseq| pseq.at(poffsets).to_a }.flatten
+    p.notes += make_notes(rhythm,pitches)
+  end
+  
+  s.parts["pluck"] = Part.new(Dynamics::MP) do |p|
+    rhythm_seq = RepeatingSequence.new(([1/8.to_r]*3)*4)
+    selector = RepeatingSequence.new([0,2,4])
+    rhythm = rhythm_seq.take(pitch_seqs.size * rhythm_seq.pattern_size).to_a
+    poffsets = selector.take(rhythm_seq.pattern_size).to_a
+    pitches = pitch_seqs.map { |pseq| pseq.at(poffsets).to_a }.flatten
+    p.notes += make_notes(rhythm,pitches)
+  end
+  
+  s.program = [0...s.measures_long]*2
+end
+
+seq = ScoreSequencer.new(score.to_timed(200)).make_midi_seq("main" => 81, "bass" => 80, "pluck" => 46)
+File.open("./part_generator.mid", 'wb'){ |fout| seq.write(fout) }
