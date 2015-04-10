3882230861340e2d3b2b2015c5211c7228bf2ddf
James Tunnell <jamestunnell@gmail.com>
2015-04-09 20:00:03 -0700

    Add #to_midi_seq convenience method Timed and TempoBased scores.

---
+++ lib/musicality/performance/midi/score_sequencing.rb
@@ -0,0 +1,17 @@
+module Musicality
+
+class Score
+  class Timed < Score
+    def to_midi_seq instr_map = {}
+      ScoreSequencer.new(self).make_midi_seq(instr_map)
+    end
+  end
+
+  class TempoBased < Score
+    def to_midi_seq tempo_sample_rate, instr_map = {}
+      to_timed(tempo_sample_rate).to_midi(instr_map)
+    end
+  end
+end
+
+end
