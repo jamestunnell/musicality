494222b340709f2fd1e6b0eadbc9e804f4a40332
James Tunnell <jamestunnell@gmail.com>
2015-04-28 20:12:26 -0700

    Add part selection and titling to Lilypond score engraver

--- lib/musicality/printing/lilypond/score_engraver.rb
+++ lib/musicality/printing/lilypond/score_engraver.rb
@@ -15,41 +15,68 @@ class ScoreEngraver
     end
 
     @parts = score.collated? ? score.parts : ScoreCollator.new(score).collate_parts
+    @title = score.title
+    @composer = score.composer
   end
 
-  def make_lilypond part_names = nil
-    part_names ||= @parts.keys
-    output = "\\version \"#{LILYPOND_VERSION}\"\n{\n  <<\n"
+  # Generate a Lilypond header for the score
+  def header
+    output = "\\version \"#{LILYPOND_VERSION}\"\n"
+    output += "\\header {\n"
+    if @title
+      output += "  title = \"#{@title}\"\n"
+    end
+    if @composer
+      output += "  composer = \"#{@composer}\"\n"
+    end
+    output += "}\n"
+
+    return output
+  end
+
+  # Generate a Lilypond staff for the given part
+  def staff part, part_title, master: false
+    clef = ScoreEngraver.best_clef(part.notes)
+    output = "  \\new Staff {\n"
+    output += "    \\set Staff.instrumentName = \\markup { \"#{part_title}\" }\n"
+    output += "    \\clef #{clef}\n"
+    if(master)
+      output += "    \\time #{@start_meter.to_lilypond}\n"
+    end
+
+    line = ""
+    part.notes.each_index do |i|
+      note = part.notes[i]
+      begin
+        str = note.to_lilypond + " "
+      rescue UnsupportedDurationError => e
+        binding.pry
+      end
+
+      if (line.size + str.size) > MAX_LINE_LEN
+        output += "    " + line
+        line = ""
+      end
+      line += str
+    end
+    output += "    " + line
+
+    output += "  }\n"
+  end
+
+  # @param [Hash] part_names A hash for titling parts differently than their names
+  # @param [Array] selected_parts The names of parts selected for engraving
+  def make_lilypond selected_parts: @parts.keys, part_titles: {}
+    (selected_parts - part_titles.keys).each do |part_name|
+      part_titles[part_name] = part_name
+    end
+    output = header
+    output += "{\n  <<\n"
     master = true
-    part_names.each do |part_name|
+    selected_parts.each do |part_name|
       part = @parts[part_name]
-      
-      clef = ScoreEngraver.best_clef(part.notes)
-      output += "  \\new Staff {\n"
-      output += "    \\clef #{clef}\n"
-      if(master)
-        output += "    \\time #{@start_meter.to_lilypond}\n"
-      end
-
-      line = ""
-      part.notes.each_index do |i|
-        note = part.notes[i]
-        begin
-          str = note.to_lilypond
-        rescue UnsupportedDurationError => e
-          binding.pry
-        end
-
-        if (line.size + str.size) > MAX_LINE_LEN
-          output += "    " + line
-          line = ""
-        end
-        line += str
-      end
-      output += "    " + line
-
-      output += "  }\n"
-      
+      part_title = part_titles[part_name]
+      output += staff part, part_title, master: master
       master = false if master
     end
     output += "  >>\n}\n"
