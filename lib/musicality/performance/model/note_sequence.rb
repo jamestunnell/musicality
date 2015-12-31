020c49fc5f2813ea70700fb893f041aec06186af
James Tunnell <jamestunnell@gmail.com>
2015-12-31 14:23:41 -0800

    Remove unused code from NoteSequence. Update tests for NoteSequence.

--- lib/musicality/performance/model/note_sequence.rb
+++ lib/musicality/performance/model/note_sequence.rb
@@ -30,6 +30,8 @@ class NoteSequence
   alias start offset
   
   def offsets
+    raise "contains no elements" if elements.empty?
+    
     off = @offset
     elements.map do |e|
       x = off
@@ -58,59 +60,9 @@ class NoteSequence
     elements.last.pitch
   end
 
-  def first_attack
-    elements.first.attack
-  end
-
   def last_attack
     elements.last.attack
   end
-
-  # attr_reader :start, :stop, :pitches, :attack
-  # def initialize start, stop, pitches, attack
-    # if start >= stop
-      # raise ArgumentError, "start #{start} is not less than stop #{stop}"
-    # end
-    
-    # if pitches.empty?
-      # raise ArgumentError, "no pitches given (at least one pitch is required at start offset)"
-    # end
-    
-    # unless pitches.has_key?(start)
-      # raise ArgumentError, "no start pitch given"
-    # end
-    
-    # pitches.keys.each do |offset|
-      # unless offset.between?(start,stop)
-        # raise ArgumentError, "pitch offset #{offset} is not between start #{start} and stop #{stop}"
-      # end
-    # end
-    
-    # @start, @stop, @pitches, @attack = start, stop, pitches, attack
-  # end
-    
-  # def self.from_elements offset, elements, attack, final_articulation
-    # pitches = {}
-    # start = offset
-    
-    # if elements.empty?
-      # raise ArgumentError, "no elements given"
-    # end
-      
-    # last = elements.last
-    # elements.each do |el|
-      # pitches[offset] = el.pitch
-      
-      # unless el.equal?(last)
-        # offset += el.duration
-      # end
-    # end
-    # stop = offset + NoteSequence.adjust_duration(last.duration, final_articulation)
-    
-    # new(start, stop, pitches, attack)
-  # end
-
-  # def duration; @stop - @start; end
 end
 
 end
--- spec/performance/model/note_sequence_spec.rb
+++ spec/performance/model/note_sequence_spec.rb
@@ -2,145 +2,52 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe NoteSequence do
   describe '#initialize' do
-    it 'should assign given start, stop, pitches and attacks' do
-      start, stop = 15, 22
-      pitches = { 15 => F2, 16 => G2, 16.1 => Ab2, 21.99 => C2 }
-      attacks = { 15 => ACCENTED, 17 => UNACCENTED, 18 => ACCENTED }
-      seq = NoteSequence.new(start,stop,pitches,attacks)
-      seq.start.should eq(start)
-      seq.stop.should eq(stop)
-      seq.pitches.should eq(pitches)
-      seq.attacks.should eq(attacks)
-    end
-    
-    it 'should raise ArgumentError if start offset >= stop offset' do
-      expect do
-        NoteSequence.new(20,19, { 20 => C4 }, { 20 => UNACCENTED })
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if no pitches are given' do
-      expect do
-        NoteSequence.new(20,21, {}, { 20 => UNACCENTED })
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if no attacks are given' do
-      expect do
-        NoteSequence.new(20,21, { 20 => C4 }, {})
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if no start pitch is given' do
-      expect do
-        NoteSequence.new(20,21, { 20.1 => C4 }, { 20 => UNACCENTED })
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if no start attack is given' do
-      expect do
-        NoteSequence.new(20,21, { 20 => C4 }, { 20.1 => UNACCENTED })
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if any pitch offset is not between start..stop' do
-      expect do
-        NoteSequence.new(20,21, { 20 => C4, 21.01 => D4 }, { 20 => UNACCENTED })
-      end.to raise_error(ArgumentError)
+    it 'should assign given offset, separation, and elements' do
+      offset = 12
+      separation = Separation::TENUTO
+      elements = [ NoteSequence::Element.new(2, G2, Attack::NORMAL) ]
       
-      expect do
-        NoteSequence.new(20,21, { 20 => C4, 19.99 => D4 }, { 20 => UNACCENTED })
-      end.to raise_error(ArgumentError)
-    end
-    
-    it 'should raise ArgumentError if any attack offset is not between start..stop' do
-      expect do
-        NoteSequence.new(20,21, { 20 => C4 }, { 20 => UNACCENTED, 21.01 => ACCENTED })
-      end.to raise_error(ArgumentError)
-      
-      expect do
-        NoteSequence.new(20,21, { 20 => C4 }, { 20 => UNACCENTED, 19.99 => ACCENTED })
-      end.to raise_error(ArgumentError)
+      seq = NoteSequence.new(offset, separation, elements)
+      expect(seq.offset).to eq(offset)
+      expect(seq.separation).to eq(separation)
+      expect(seq.elements).to eq(elements)
     end
   end
+
+  before :all do
+    @element_arys = [
+      [ NoteSequence::Element.new(0.5, B2, Attack::NONE) ],
+      [ NoteSequence::Element.new(0.5, A2, Attack::NORMAL), 
+        NoteSequence::Element.new(0.5, B2, Attack::NONE),
+        NoteSequence::Element.new(0.5, B2, Attack::ACCENT) ],
+    ]
+    @offsets = [ 0, -5, 7, 77 ]
+  end
   
-  describe '.from_elements' do
-    it 'should raise ArgumentError if no elements are given' do
-      expect { NoteSequence.from_elements(2,[]) }.to raise_error(ArgumentError)
-    end
-    
-    context 'single element' do
-      before :all do
-        @offset = 0
-        @el = FinalElement.new(2, C2, true, NORMAL)
-        @seq = NoteSequence.from_elements(@offset, [ @el ])
-        
-      end
-      
-      it 'should return a NoteSequence' do
-        @seq.should be_a NoteSequence
-      end
-      
-      it 'should set start offset to given offset' do
-        @seq.start.should eq(@offset)
-      end
-      
-      it 'should set stop offset no more than "duration of first element" away from start' do
-        (@seq.stop - @seq.start).should be <= @el.duration
-      end
-      
-      it 'should set start pitch according to element pitch' do
-        @seq.pitches[@seq.start].should eq(@el.pitch)
-      end
-      
-      it 'should set start attack according to element.accented' do
-        @seq.attacks[@seq.start].accented?.should eq(@el.accented)
+  describe '#offsets' do
+    context 'with no elements' do
+      it 'should raise RuntimeError' do
+        expect do
+          seq = NoteSequence.new(0, Separation::NORMAL, [])
+          seq.offsets
+        end.to raise_error(RuntimeError)
       end
     end
     
-    context 'multi-element' do
-      before :all do
-        @offset = 1.5
-        @els = [
-          SlurredElement.new(1.0, A2, false),
-          LegatoElement.new(1.1, B2, false),
-          SlurredElement.new(1.2, C2, false),
-          LegatoElement.new(1.3, B2, false),
-          FinalElement.new(1.4, A2, false, NORMAL)
-        ]
-        @seq = NoteSequence.from_elements(@offset, @els)
-      end
-      
-      it 'should place pitches according to element duration' do
-        offset = @offset
-        @els.each do |el|
-          @seq.pitches.should have_key(offset)
-          @seq.pitches[offset].should eq(el.pitch)
-          offset += el.duration
-        end
-      end
-      
-      it 'should place attacks at beginning and following non-slur elements' do
-        @seq.attacks.should have_key(@offset)
-        
-        offset = @offset + @els.first.duration
-        (1...@els.size).each do |i|
-          unless @els[i-1].slurred?
-            @seq.attacks.should have_key(offset)
+    context 'with at least one element' do
+      it 'should return offsets of each element' do
+        @offsets.each do |offset|
+          @element_arys.each do |els|
+            seq = NoteSequence.new(offset, Separation::NORMAL, els)
+            offsets = seq.offsets
+            expect(offsets.size).to eq(els.size)
+            expect(offsets[0]).to eq(seq.offset)
+            (1...offsets.size).each do |i|
+              dur = offsets[i] - offsets[i-1]
+              expect(dur).to eq(els[i-1].duration)
+            end
           end
-          offset += @els[i].duration
         end
-      end
-    end
-    
-    context 'elements contain slur to same pitch' do
-      it 'should not add same pitch nor attack for second element' do
-        els = [ SlurredElement.new(1, C4, false), FinalElement.new(1, C4, false, NORMAL) ]
-        seq = NoteSequence.from_elements(0, els)
-        seq.pitches.should have_key(0)
-        seq.pitches.should_not have_key(1)
-        seq.attacks.should have_key(0)
-        seq.attacks.should_not have_key(1)
       end
     end
   end
