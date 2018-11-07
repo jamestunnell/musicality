192910931fcf024dad215cdb6903ea492ba4e3e3
James Tunnell <jamestunnell@gmail.com>
2018-11-07 14:51:17 -0800

    Increase the values of dynamic levels

--- lib/musicality/notation/model/dynamics.rb
+++ lib/musicality/notation/model/dynamics.rb
@@ -1,7 +1,7 @@
 module Musicality
 module Dynamics
-  PPP = 0.05
-  FFF = 0.5
+  PPP = 0.1
+  FFF = 0.9
   DYNAMIC_RATIO = (FFF/PPP)**(1.0/7.0) # 7 ratios between the 8 dynamic levels
 
   PP = PPP*DYNAMIC_RATIO
